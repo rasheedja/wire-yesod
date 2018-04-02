674b3eddcf6cb3e9714e45713a66877c1e9d6c63
Junaid Rasheed <jrasheed178@gmail.com>
2018-04-02 14:03:16 +0100

    Test the following handler
    + Also fix typo in FollwersSpec

--- yesod/wire/test/Handler/FollowingSpec.hs
+++ yesod/wire/test/Handler/FollowingSpec.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Handler.FollowersSpec (spec) where
+module Handler.FollowingSpec (spec) where
 
 import           Database.Persist.Sql (fromSqlKey)
 import           TestImport
@@ -8,49 +8,47 @@ import           TestImport
 spec :: Spec
 spec = withApp $ do
 
-    describe "getFollowersR" $ do
+    describe "getFollowingR" $ do
         it "asserts access for anonymous users" $ do
-            get $ FollowersR "foo"
+            get $ FollowingR "foo"
             statusIs 200
 
         it "asserts correct message is shown when looking up non-existant users" $ do
-            get $ FollowersR "foo"
+            get $ FollowingR "foo"
             bodyContains "false"
             bodyContains "The given username was not found"
 
-        it "asserts that the request returns nothing if nobody follows a user" $ do
+        it "asserts that the request returns nothing if a user is not following anyone" $ do
             _ <- createUser "foo" "foo@bar.com" "foo"
 
-            get $ FollowersR "foo"
+            get $ FollowingR "foo"
             bodyNotContains "followerId"
             bodyNotContains "followingId"
 
-        it "asserts that the request returns the correct data if somebody follows a user" $ do
-            _ <- createUser "foo" "foo@bar.com" "foo"
+        it "asserts that the request returns the correct data if a user follows someone" $ do
+            foo <- createUser "foo" "foo@bar.com" "foo"
             bar <- createUser "bar" "bar@bar.com" "foo"
             baz <- createUser "baz" "baz@bar.com" "foo"
 
-            authenticateAs bar
-            get $ FollowR "foo"
+            authenticateAs foo
+            get $ FollowR "bar"
 
-            get $ FollowersR "foo"
+            get $ FollowingR "foo"
             bodyContains "followerId"
             bodyContains "followingId"
             bodyContains $ show $ fromSqlKey $ entityKey bar
             bodyNotContains $ show $ fromSqlKey $ entityKey baz
 
-        it "asserts that the request returns the correct data if multiple people follow a user" $ do
-            _ <- createUser "foo" "foo@bar.com" "foo"
+        it "asserts that the request returns the correct data if a user follows multiple people" $ do
+            foo <- createUser "foo" "foo@bar.com" "foo"
             bar <- createUser "bar" "bar@bar.com" "foo"
             baz <- createUser "baz" "baz@bar.com" "foo"
 
-            authenticateAs bar
-            get $ FollowR "foo"
+            authenticateAs foo
+            get $ FollowR "bar"
+            get $ FollowR "baz"
 
-            authenticateAs baz
-            get $ FollowR "foo"
-
-            get $ FollowersR "foo"
+            get $ FollowingR "foo"
             bodyContains "followerId"
             bodyContains "followingId"
             bodyContains $ show $ fromSqlKey $ entityKey bar
