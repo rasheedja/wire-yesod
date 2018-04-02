9c527e09bf817d94d8a6ec65a490fd6dce3fe31a
Junaid Rasheed <jrasheed178@gmail.com>
2018-04-02 15:53:24 +0100

    Test the Profile handler

--- yesod/wire/test/Handler/ProfileSpec.hs
+++ yesod/wire/test/Handler/ProfileSpec.hs
@@ -2,12 +2,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Handler.ProfileSpec (spec) where
 
-import TestImport
+import           TestImport
 
 spec :: Spec
 spec = withApp $ do
 
-    describe "Profile page" $ do
-        it "asserts no access to my-account for anonymous users" $ do
-            get ProfileR
+    describe "getProfileR" $ do
+        it "asserts access for anonymous users" $ do
+            _ <- createUser "foo" "foo@bar.com" "foo"
+            get $ ProfileR "foo"
+            statusIs 200
+
+        it "asserts error message is shown when accessing non-existant user's page" $ do
+            get $ ProfileR "foo"
+
             statusIs 303
+            _ <- followRedirect
+            statusIs 200
+
+            htmlAnyContain ".alert-danger > span" "Username not found"
+
+        it "asserts profile page is correctly rendered for existing users" $ do
+            _ <- createUser "foo" "foo@bar.com" "foo"
+            get $ ProfileR "foo"
+            statusIs 200
+
+            htmlAnyContain "h3" "foo"
+            htmlAnyContain "h3" "Wires"
+            htmlAnyContain "h3" "Followers"
+            htmlAnyContain "h3" "Following"
+            htmlAnyContain "h3" "Other Users"
