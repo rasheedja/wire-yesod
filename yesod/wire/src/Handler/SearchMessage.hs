f432aaac218c28ec8659711eb7b3fa8635be6907
Junaid Rasheed <jrasheed178@gmail.com>
2018-04-06 23:40:47 +0100

    Use new message helper for message search results

--- yesod/wire/src/Handler/SearchMessage.hs
+++ yesod/wire/src/Handler/SearchMessage.hs
@@ -6,6 +6,7 @@
 
 module Handler.SearchMessage where
 
+import           Helper.Message
 import           Import
 
 getSearchMessageR :: Text -> Handler Html
@@ -13,6 +14,8 @@ getSearchMessageR query = do
     results <- runDB $ selectList [Filter MessageMessage (Left $ Import.concat ["%", query, "%"]) (BackendSpecificFilter "ILIKE")] []
     let posterIds = Import.map (\(Entity _ (Message _ posterId _)) -> posterId) results
     posters <- runDB $ selectList [UserId <-. posterIds] []
+
+    let formattedResults = Import.map (formatMessageEntity posters) results
 
     defaultLayout $ do
         setTitle "Wire Search Results"
